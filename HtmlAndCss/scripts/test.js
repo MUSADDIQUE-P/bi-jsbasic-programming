a17432cf1a582d3106189d62a9d72630ddcb90d6
MUSADDIQUE-P <musaddiquepinjari@gmail.com>
2022-07-09 20:24:55 +0530

    test

---
+++ HtmlAndCss/scripts/test.js
@@ -0,0 +1,7 @@
+fname="Musaddique"
+spec="MERN Stack"
+age=21
+year=1
+console.log(age+year)
+console.log(`Candidate ${fname} he is specilized in ${spec} he is ${age} young`)
+alert(`Welcome to BI ${fname}  ! happy learning `)
