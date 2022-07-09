2c768cd86d3cb6bec0fad85cb9b7ad800d2caa3f
MUSADDIQUE-P <101107903+MUSADDIQUE-P@users.noreply.github.com>
2022-07-09 20:41:53 +0530

    Delete HtmlAndCss directory

--- HtmlAndCss/scripts/test.js
+++
@@ -1,7 +0,0 @@
-fname="Musaddique"
-spec="MERN Stack"
-age=21
-year=1
-console.log(age+year)
-console.log(`Candidate ${fname} he is specilized in ${spec} he is ${age} young`)
-alert(`Welcome to BI ${fname}  ! happy learning `)
