8430d55e4aedd2c7f94de21704a7ba40ebf166a5
MUSADDIQUE-P <101107903+MUSADDIQUE-P@users.noreply.github.com>
2022-07-13 16:42:37 +0530

    Update problem4.js

--- problem4.js
+++ problem4.js
@@ -2,5 +2,5 @@ var getDaysInMonth = function(month,year)
 {
  return new Date(year, month, 0).getDate();
 };
-console.log(getDaysInMonth(8, 2022));
-console.log(getDaysInMonth(2, 2022));
+document.write(getDaysInMonth(8, 2022));
+document.write(getDaysInMonth(2, 2022));
